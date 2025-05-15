2a5f16149b00a65db0e0b69c9141000e33f04e30
justin23bohrer <91996601+justin23bohrer@users.noreply.github.com>
2025-05-15 15:14:32 -0500

    42 game works with ai

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,6 +7,7 @@ const SECOND_PLAYER = Vector2(604, 360)
 const THIRD_PLAYER = Vector2(672, 360)
 const FOURTH_PLAYER = Vector2(740, 360)
 
+var leadingSuit = -1
 var myScore = 0
 var opponentScore = 0
 var dominosInMiddle = []
@@ -35,6 +36,8 @@ func turn(turn, player):
 		await $"../Timer".timeout
 		ai_plays_dom(player, placement)
 	else:
+		$"../Timer".start()
+		await $"../Timer".timeout
 		$"../dominoSlot".visible = true
 		$"../dominoSlot".position = placement
 		$"../subDom".visible = true
@@ -47,17 +50,19 @@ func play_round():
 			"op2": [],
 			"me": []
 		}
+		leadingSuit = 0
 		rotate_players()
 		for y in player_rotation.size():
 			var player = player_rotation[y]
 			await turn(y, player)
 			if player == "me":
 				await self.domino_submitted
-			$"../Timer".start()
-			await $"../Timer".timeout
+			if y == 0 and domsThatTurn[player].size() > 0:
+				var played_dom = domsThatTurn[player][0]
+				leadingSuit = max(played_dom[0], played_dom[1])
 		domino_accountant()
-		$"../endRound".start()
-		await $"../endRound".timeout
+		$"../Timer".start()
+		await $"../Timer".timeout
 	#clear doms
 		for j in dominosInMiddle:
 			j.queue_free()
@@ -128,62 +133,92 @@ func _on_sub_dom_pressed() -> void:
 
 	#submitting dom in slot
 	var domino = slot.domino
-	dominosInMiddle.append(domino)
 	domsThatTurn["me"].append([domino.left_value, domino.right_value])
+	emit_signal("domino_submitted")
+	dominosInMiddle.append(domino)
 	slot.domino_in_slot = false
 	slot.get_node("Area2D/CollisionShape2D").disabled = false
-
-	emit_signal("domino_submitted")
+	
 
 func ai_plays_dom(player, location):
 	var hand_node
 	match player:
-		"op1":
-			hand_node = get_node("../opponentHand1")
-		"op2":
-			hand_node = get_node("../opponentHand2")
-		"tm8":
-			hand_node = get_node("../teammateHand")
-		_:
-			print("Something terribly wrong has happen")
-	if hand_node.player_hand.size() > 0:
-		var highest_domino = hand_node.player_hand[0]
-		for domino in hand_node.player_hand:
-			if domino.left_value + domino.right_value > highest_domino.left_value + highest_domino.right_value:
-				highest_domino = domino
+		"op1": hand_node = get_node("../opponentHand1")
+		"op2": hand_node = get_node("../opponentHand2")
+		"tm8": hand_node = get_node("../teammateHand")
+		_: print("Invalid AI player"); return
 
-		hand_node.present_domino(highest_domino, location)
-		highest_domino.update_domino_display()
-		dominosInMiddle.append(highest_domino)
-		match player:
-			"op1":
-				domsThatTurn["op1"].append([highest_domino.left_value, highest_domino.right_value])
-			"op2":
-				domsThatTurn["op2"].append([highest_domino.left_value, highest_domino.right_value])
-			"tm8":
-				domsThatTurn["tm8"].append([highest_domino.left_value, highest_domino.right_value])
-			_:
-				print("Something terribly wrong has happen")
+	var legal_dominos = []
+	var off_suit_dominos = []
+
+	for domino in hand_node.player_hand:
+		if domino.left_value == leadingSuit or domino.right_value == leadingSuit:
+			legal_dominos.append(domino)
+		else:
+			off_suit_dominos.append(domino)
+
+	var chosen_domino
+	if legal_dominos.size() > 0:
+		# Prioritize doubles first, then highest sum
+		chosen_domino = legal_dominos[0]
+		for dom in legal_dominos:
+			if dom.left_value == dom.right_value and chosen_domino.left_value != chosen_domino.right_value:
+				chosen_domino = dom
+			elif (dom.left_value + dom.right_value) > (chosen_domino.left_value + chosen_domino.right_value):
+				chosen_domino = dom
+	else:
+		# No legal move, play lowest off-suit domino
+		chosen_domino = off_suit_dominos[0]
+		for dom in off_suit_dominos:
+			if (dom.left_value + dom.right_value) < (chosen_domino.left_value + chosen_domino.right_value):
+				chosen_domino = dom
+
+	hand_node.present_domino(chosen_domino, location)
+	chosen_domino.update_domino_display()
+	dominosInMiddle.append(chosen_domino)
+	domsThatTurn[player].append([chosen_domino.left_value, chosen_domino.right_value])
 
 func domino_accountant():
 	print(domsThatTurn)
 	winning_player = get_player_with_biggest_domino()
+	
+	var points_this_round = 0
+	for domino in dominosInMiddle:
+		var sum = domino.left_value + domino.right_value
+		if sum % 5 == 0:
+			points_this_round += sum
+	
 	if winning_player == "me" or winning_player == "tm8":
-		myScore += 1
+		myScore += points_this_round+1
 	if winning_player == "op1" or winning_player == "op2":
-		opponentScore += 1
+		opponentScore += points_this_round+1
 	update_score()
 	
 func get_player_with_biggest_domino() -> String:
-	var highest_sum = -1
 	var best_player = ""
+	var best_score = -1
+	var best_priority = -1
 
 	for player in domsThatTurn.keys():
-		for domino in domsThatTurn[player]:
-			var domino_sum = domino[0] + domino[1]
-			if domino_sum > highest_sum:
-				highest_sum = domino_sum
-				best_player = player
+		var dom = domsThatTurn[player][0]
+		var is_double = dom[0] == dom[1]
+		var follows_suit = dom[0] == leadingSuit or dom[1] == leadingSuit
+		var dom_score = dom[0] + dom[1]
+
+		# Priority: double-in-suit > in-suit > double > off-suit
+		var priority = 0
+		if is_double and follows_suit:
+			priority = 3
+		elif follows_suit:
+			priority = 2
+		elif is_double:
+			priority = 1
+
+		# Pick higher priority, or higher sum within tie
+		if priority > best_priority or (priority == best_priority and dom_score > best_score):
+			best_priority = priority
+			best_score = dom_score
+			best_player = player
 
 	return best_player
 
